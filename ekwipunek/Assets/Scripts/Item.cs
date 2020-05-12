accb83fb42cb2e148225ac6b69bdbfe3902e5f9b
awro1444 <57668665+awro1444@users.noreply.github.com>
2020-05-12 16:38:54 +0200

    pickUp, inventory
    podnoszenie i opuszczanie rzeczy, widok ekwipunku

---
+++ ekwipunek/Assets/Scripts/Item.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item
+{
+    public enum ItemType
+    {
+        Sword,
+        Apple,
+    }
+
+    public ItemType itemType;
+    public int amount;
+}
