0c0927c5301908ccbf3f0a486bd9ecdb6d1c5046
Rozanna <rozanna.boczar@gmail.com>
2020-05-15 19:43:06 +0100

    door not done

---
+++ Assets/Scripts/OpenSlidingDoor1.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenSlidingDoor1 : MonoBehaviour
+{
+    private Animator _animator;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        _animator = GetComponent<Animator>();
+    }
+
+    void OnTriggerEnter(Collider player)
+    {
+        if (player.gameObject.tag == "MC")
+        {
+            _animator.SetBool("open", true);
+            //Destroy(colaCan);
+           // StartCoroutine("WaitForSec");
+            //_animator.SetBool("open", false);
+
+        }
+
+    }
+
+    //IEnumerator WaitForSec()
+   // {
+        //yield return new WaitForSeconds(2);
+       // Destroy(colaCan);
+        //Destroy(gameObject); info tylko raz
+
+    //}
+
+}
