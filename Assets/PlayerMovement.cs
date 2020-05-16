e903855a7d9199c409c14e0a247efc71909cc404
Aleksandra <49525674+panda020@users.noreply.github.com>
2020-05-16 15:55:02 +0200

    skakanie

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -7,12 +7,26 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
     public float speed = 12f;
     public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+    int HowManyJumps = 1;
 
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
+    
+    
     Vector3 velocity;
+    bool isGrounded;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.Raycast(transform.position, Vector3.down, 2);
+        if (isGrounded && velocity.y < 0)
+        {
+            HowManyJumps = 1;
+            velocity.y = -2f;
+        }
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
@@ -20,7 +34,14 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        if (Input.GetButtonDown("Jump") && isGrounded && HowManyJumps > 0) {
+            print(HowManyJumps);
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+            HowManyJumps--;
+        }
+
         velocity.y += gravity + Time.deltaTime;
         controller.Move(velocity * Time.deltaTime);
     }
+
 }
