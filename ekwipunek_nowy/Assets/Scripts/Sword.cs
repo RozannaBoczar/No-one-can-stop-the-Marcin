2fb7bad86bf59a4290d6d9d3c3de567d0dcd0596
awro1444 <57668665+awro1444@users.noreply.github.com>
2020-05-16 11:57:43 +0200

    poprawki

---
+++ ekwipunek_nowy/Assets/Scripts/Sword.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Sword : MonoBehaviour, IInventoryItem
+{
+    public string Name
+    {
+        get
+        {
+            return "Sword";
+        }
+    }
+
+    public Sprite _Image = null;
+
+    public Sprite Image
+    {
+        get
+        {
+            return _Image;
+        }
+    }
+
+    public void OnPickup()
+    {
+        gameObject.SetActive(false);
+    }
+
+    public void OnDrop()
+    {
+        print("on drop2");
+        RaycastHit hit = new RaycastHit();
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 1000))
+        {
+            print("on drop");
+            gameObject.SetActive(true);
+            gameObject.transform.position = hit.point;
+
+        }
+    }
+}
+
