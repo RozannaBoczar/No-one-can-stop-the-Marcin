429386ee8a9661e62efd8e12646bd86122f52c19
Ina20 <inaszkola@gmail.com>
2020-05-19 20:59:19 +0200

    router

--- Assets/TurnOn.cs
+++ Assets/TurnOn.cs
@@ -7,24 +7,18 @@ public class TurnOn : MonoBehaviour
 
     public Material[] material;
     Renderer rend;
-    public GameObject cabel;
 
-    // Start is called before the first frame update
     void Start()
     {
         rend = GetComponent<Renderer>();
         rend.enabled = true;
         rend.sharedMaterial = material[0];
     }
+    
 
-    // Update is called once per frame
-    void OnMouseDown(Collider player)
+    void OnMouseDown()
     {
-            if(player.gameObject.tag == "cabel"){
-                print("Kabel click!");
-                rend.material = material[1];
-            }else{
-                rend.material = material[0];
-            }
+        rend.material = material[1];
+
     }
 }
