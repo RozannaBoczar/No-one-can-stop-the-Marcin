9a4bb43d66c6a5767ef61b78c71499a54ad89c16
Ina20 <inaszkola@gmail.com>
2020-05-19 19:47:33 +0200

    router

---
+++ Assets/TurnOn.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TurnOn : MonoBehaviour
+{
+
+    public Material[] material;
+    Renderer rend;
+    public GameObject cabel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rend = GetComponent<Renderer>();
+        rend.enabled = true;
+        rend.sharedMaterial = material[0];
+    }
+
+    // Update is called once per frame
+    void OnMouseDown(Collider player)
+    {
+            if(player.gameObject.tag == "cabel"){
+                print("Kabel click!");
+                rend.material = material[1];
+            }else{
+                rend.material = material[0];
+            }
+    }
+}
